53c09dfb977634a54fcb179751a1271ca48b88fd
Sahil Kale <sahil.kale1@gmail.com>
2025-01-19 15:42:22 -0500

    Mixing valve test

--- mixing-valve-test/src/main.cpp
+++ mixing-valve-test/src/main.cpp
@@ -5,8 +5,6 @@ uint8_t nSLEEP = 23;
 uint8_t solenoidPin = 27;
 uint8_t pHPin = 35;
 uint8_t tdsPin = 36; //analog input, TODO FILL IN
-uint8_t delayTimeMSDefault = 50;
-
 
 void setup() {
   Serial.begin(9600);
@@ -18,6 +16,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+
   if (Serial.available()>0){
     // Expect a string of form "0" to "1024", corresponding to the time to run the solenoid
     // in milliseconds
@@ -25,7 +24,7 @@ void loop() {
     uint16_t delayTimeMS = Serial.parseInt();
 
     if (delayTimeMS == 0){
-      delayTimeMS = delayTimeMSDefault;
+        return;
     }
 
     Serial.print("Actuating valve for ");
@@ -35,10 +34,11 @@ void loop() {
     digitalWrite (solenoidPin,HIGH);
     delay(delayTimeMS);
     digitalWrite (solenoidPin, LOW);
+    Serial.println("Finished actuating valve");
   }
-  uint8_t tdsValue = analogRead(tdsPin);
 
 #ifdef PRINT_TDS_VALUE  
+    uint8_t tdsValue = analogRead(tdsPin);
   constexpr float aioToVolts = 3.3 / 4095.0;
 
   Serial.print("TDS value: ");
@@ -46,6 +46,6 @@ void loop() {
 
   delay(1000);
 #else
- delay(10);
+ delay(100);
 #endif
 }
